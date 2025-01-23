df51cc241ff4fcf6c0ca30a8f53a540127637039
Theo Virelaude <theo.virelaude@avisto.com>
2025-01-23 16:04:10 +0000

    Added -f arg to specify the filepath of the docker compose file.

--- import_db_backup.sh
+++ import_db_backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Parse command line arguments
-while getopts "a:s:r:u:v:n:" opt; do
+while getopts "a:s:r:u:v:n:f:" opt; do
   case $opt in
     a) aws_access_key="$OPTARG";;
     s) aws_secret_key="$OPTARG";;
@@ -9,20 +9,21 @@ while getopts "a:s:r:u:v:n:" opt; do
     u) s3_url="$OPTARG";;
     v) volume_name="$OPTARG";;
     n) service="$OPTARG";;
-    ?) echo "Usage: $0 -a AWS_ACCESS_KEY -s AWS_SECRET_KEY -r AWS_REGION -u S3_BACKUP_URL -v VOLUME_NAME -n SERVICE_NAME" >&2
+    f) compose_filepath="$OPTARG";;
+    ?) echo "Usage: $0 -a AWS_ACCESS_KEY -s AWS_SECRET_KEY -r AWS_REGION -u S3_BACKUP_URL -v VOLUME_NAME -n SERVICE_NAME -f COMPOSE_FILEPATH" >&2
        exit 1;;
   esac
 done
 
 # Verify all required arguments are provided
-if [ -z "$aws_access_key" ] || [ -z "$aws_secret_key" ] || [ -z "$aws_region" ] || [ -z "$s3_url" ] || [ -z "$volume_name" ] || [ -z "$service" ]; then
+if [ -z "$aws_access_key" ] || [ -z "$aws_secret_key" ] || [ -z "$aws_region" ] || [ -z "$s3_url" ] || [ -z "$volume_name" ] || [ -z "$service" ] || [ -z "$compose_filepath" ]; then
     echo "Error: Missing required arguments"
-    echo "Usage: $0 -a AWS_ACCESS_KEY -s AWS_SECRET_KEY -r AWS_REGION -u S3_BACKUP_URL -v VOLUME_NAME -n SERVICE_NAME"
+    echo "Usage: $0 -a AWS_ACCESS_KEY -s AWS_SECRET_KEY -r AWS_REGION -u S3_BACKUP_URL -v VOLUME_NAME -n SERVICE_NAME -f COMPOSE_FILEPATH"
     exit 1
 fi
 
 # Down container
-docker compose down $service
+docker compose -f $compose_filepath down $service
 
 # Run side container --rm + mount volume
 # Odo handles the restoration of the backup
@@ -34,7 +35,7 @@ docker run -d --rm \
   -v $volume_name:/data odo:0.1
 
 # Up container
-docker compose up -d $service
+docker compose -f $compose_filepath up -d $service
 
 # Promote master
 docker exec -it -u postgres $CONTAINER_NAME bash -c "pg_ctl promote"
